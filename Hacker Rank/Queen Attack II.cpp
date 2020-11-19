c160c54756d596e659ccda51f04746d0fc1a8023
AibaAina <46571993+AibaAina@users.noreply.github.com>
2020-11-19 21:03:15 +0800

    11/19/2020 - record

--- Hacker Rank/Queen Attack II.cpp
+++ Hacker Rank/Queen Attack II.cpp
@@ -12,35 +12,47 @@
 using namespace std;
 
 int queensAttack(int n, int k, int r_q, int c_q, vector<vector<int>> obstacles) {
-    
-    int ans=0;
 
-    vector<vector<int>>board(n+1, vector<int>(n+1, 0));
-    for (vector<int> v:obstacles) board[v[0]][v[1]]=1;
-    board[r_q][c_q]=1;
+    int top = r_q-1;
+    int bottom = n-r_q;
+    int right = n-c_q;
+    int left = c_q-1;
 
-    int i,j;
-    
-    // vertical
-    for (i=1, j=c_q ; i<=n ; i++) 
-        if (board[i][j]==0) ans++;
-    // horizon
-    for (i=r_q, j=1 ; j<=n ; j++) 
-        if (board[i][j]==0) ans++;
-    // left top
-    for (i=r_q, j=c_q ; i>0 && j>0 ; i--, j--) 
-        if (board[i][j]==0) ans++;
-    // right top
-    for (i=r_q, j=c_q ; i>0 && j<=n ; i--, j++) 
-        if (board[i][j]==0) ans++;
-    // left down
-    for (i=r_q, j=c_q ; i<=n && j>0 ; i++, j--) 
-        if (board[i][j]==0) ans++;
-    // right down
-    for (i=r_q, j=c_q ; i<=n && j<=n ; i++, j++) 
-        if (board[i][j]==0) ans++;
-    
-    return ans;
+    int tr = min(top, right);
+    int br = min(bottom, right);
+    int bl = min(bottom, left);
+    int tl = min(top, left);
+
+    for (vector<int> v : obstacles) {
+
+        if (v[0]==r_q) {
+            if (v[1]<c_q)  left = min(left, c_q-v[1]-1);
+            else right = min(right, v[1]-c_q-1);
+        }
+        else if (v[1]==c_q) {
+            if (v[0]<r_q) top = min(top, r_q-v[0]-1);
+            else bottom = min(bottom, v[0]-r_q-1);
+        }
+
+        else if (abs(v[0]-r_q) == abs(v[1]-c_q)) {
+            // top left
+            if (v[0]<r_q && v[1]<c_q)
+                tl = min(tl, r_q-v[0]-1);
+
+            // top right
+            else if (v[0]<r_q && v[1]>c_q) 
+                tr = min(tr, r_q-v[0]-1);
+
+            // bottom left
+            else if (v[0]>r_q && v[1]<c_q)
+                bl = min(bl, v[0]-r_q-1);
+
+            // bottom right
+            else if (v[0]>r_q && v[1]>c_q)
+                br = min(br, v[0]-r_q-1);
+        }
+    }
+    return top+bottom+right+left+tl+tr+bl+br;
 }
 
 int main() {
@@ -48,7 +60,7 @@ int main() {
     int n=5;
     int k=3;
     int r_q=4, c_q=3;
-    vector<vector<int>> obstacles = {{5,5},{4,2},{2,3}};
+    vector<vector<int>> obstacles = {{5,5},{4,2},{2,3},{2,5},{2,1},{5,2},{5,4}};
 
     cout<<queensAttack(n,k,r_q,c_q,obstacles);
 
