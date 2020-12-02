48985c144730ad1d5898d7f7f91267195ab244ce
AibaAina <46571993+AibaAina@users.noreply.github.com>
2020-12-02 16:15:54 +0800

    12/02/2020 - record

--- Hacker Rank/Ice Cream Parlor.cpp
+++ Hacker Rank/Ice Cream Parlor.cpp
@@ -5,32 +5,32 @@
 
 #include <vector>
 #include <queue>
+#include <stack>
 #include <map>
 #include <unordered_map>
 
 using namespace std;
 
-int main() {
+void whatFlavors(vector<int> cost, int money) {
 
-    vector<int> cost={4,3,2,5,7};
-    int money = 8;
+    unordered_map<int, int>dict;
 
-    unordered_map<int, int> map;
-    vector<int> ans;
-    
+    for (int i=0 ; i<cost.size() ; i++) dict[cost[i]]=i;
     for (int i=0 ; i<cost.size() ; i++) {
-        map[cost[i]]=i;
-    }
-    
-    for (int i=0 ; i<cost.size() ; i++) {
-        int t=money-cost[i];
-        if (map.count(t) && map[t]!=i) {
-            ans.push_back(i);
-            ans.push_back(map[t]);
-            break;
+        int tar = money-cost[i];
+        if (dict[tar]>0 && dict[tar]!=i) {
+            cout<<i+1<<" "<<dict[tar]+1<<endl;
         }
     }
+}
 
-    cout<<ans[0]+1<<" "<<ans[1]+1<<endl;
+int main() {
+
+    vector<int> cost = {1, 4, 5, 3, 2};
+    int money=4;
+    string s = "hgygsvlfwcwnswtuhmyaljkqlqjjqlqkjlaymhutwsnwcflvsgygh";
+    whatFlavors(cost, money);
+
     return 0;
 }
+
--- Reviewed.md
+++ Reviewed.md
@@ -4,3 +4,7 @@ Non-Divisible Subset
 Special String Again  
 3D Surface Area  
 Two Characters  
+
+Larry's Array  
+Palindrome Index  
+Hash Tables: Ice Cream Parlor  
